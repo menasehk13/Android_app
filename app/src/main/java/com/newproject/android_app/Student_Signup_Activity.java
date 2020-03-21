0e78465f192a4a8fca08a51c1d47cf99a5a080f0
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-03-21 22:48:04 +0300

    finished the student signup, mechamerawu kale chemerebet

--- app/src/main/java/com/newproject/android_app/Student_Signup_Activity.java
+++ app/src/main/java/com/newproject/android_app/Student_Signup_Activity.java
@@ -1,8 +1,10 @@
 package com.newproject.android_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,6 +13,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -29,64 +33,134 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 
-public class Student_Signup_Activity extends AppCompatActivity implements AsyncResponse, View.OnClickListener {
-    EditText firstname, lastname, password, confirm_password, registered_year, student_id;
-    Spinner gender, department, section;
-    Button submit;
-    String dep[] = {"Computer Engineering", "Computer Science", "TVET", "Accounting"};
-    String sec[] = {"1", "2", "3", "4"};
-    String gend[] = {"MALE", "FEMALE"};
+public class Student_Signup_Activity extends AppCompatActivity implements  View.OnClickListener {
 
+    EditText firstname,lastname,gender,department,section,year,id,pass;
+   RadioGroup radioGroup;
+    Button register;
+    Spinner dep,sec;
+    String deprt[]={"Computer Engineering","Computer Science","Accounting","TVTE"};
+    String secs[]={"1","2","3"};
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student__signup_);
-        firstname = findViewById(R.id.First_name);
-        lastname = findViewById(R.id.last_name);
-        password = findViewById(R.id.student_password);
-        confirm_password = findViewById(R.id.confirm_password1);
-        registered_year = findViewById(R.id.year_registered);
-        student_id = findViewById(R.id.Micro_id);
-        gender = findViewById(R.id.gender_spinner);
-        department = findViewById(R.id.department_spinner);
-        section = findViewById(R.id.section_spinner);
-        submit = findViewById(R.id.submit);
-        ArrayAdapter gender_Array = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, gend);
-        gender.setAdapter(gender_Array);
-        ArrayAdapter department_array = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, dep);
-        department.setAdapter(department_array);
-        ArrayAdapter Setion_array = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, sec);
-        section.setAdapter(Setion_array);
-        submit.setOnClickListener(this);
-
+      firstname=findViewById(R.id.first);
+      lastname=findViewById(R.id.last);
+      dep=findViewById(R.id.departement);
+      sec=findViewById(R.id.section);
+      ArrayAdapter adapter=new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,secs);
+      sec.setAdapter(adapter);
+      radioGroup=findViewById(R.id.gendergroup);
+        ArrayAdapter arrayAdapter=new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,deprt);
+        dep.setAdapter(arrayAdapter);
+      year=findViewById(R.id.year_register);
+      id=findViewById(R.id.studentid);
+      pass=findViewById(R.id.pass);
+      register=findViewById(R.id.finish);
+      register.setOnClickListener(this);
+      int selecteditem=radioGroup.getCheckedRadioButtonId();
+      RadioButton radioButton=findViewById(radioGroup.getCheckedRadioButtonId());
+      String gender=radioButton.getText().toString();
     }
 
     @Override
     public void onClick(View view) {
-      /*  String Firstname = firstname.getText().toString();
-        String Lastname = lastname.getText().toString();
-        String Department = department.getSelectedItem().toString();
-        String Gender = gender.getSelectedItem().toString();
-        String Section = section.getSelectedItem().toString();
-        String Pass = password.getText().toString();
-        String year = registered_year.getText().toString();
-        */
-        HashMap hashMap=new HashMap();
-        hashMap.put("First_name",firstname.getText().toString());
-        hashMap.put("Last_name",lastname.getText().toString());
-        hashMap.put("Gender",gender.getSelectedItem().toString());
-        hashMap.put("Departement",department.getSelectedItem().toString());
-        hashMap.put("Section",section.getSelectedItem().toString());
-        hashMap.put("StudentId",student_id.getText().toString());
-        hashMap.put("Password",password.getText().toString());
-        PostResponseAsyncTask responseAsyncTask=new PostResponseAsyncTask(this,hashMap);
-        responseAsyncTask.execute("http://192.168.0.104/Micro/studentsignup.php");
+            /*HashMap hashMap = new HashMap();
+            hashMap.put("mobile","android");
+            hashMap.put("First_name", firstname.getText().toString());
+            hashMap.put("Last_name", lastname.getText().toString());
+            hashMap.put("Gender",gender.getText().toString());
+            hashMap.put("Departement", department.getText().toString());
+            hashMap.put("Year", year.getText().toString());
+            hashMap.put("Section", section.getText().toString());
+            hashMap.put("StudentId", id.getText().toString());
+            hashMap.put("Password", pass.getText().toString());
+            PostResponseAsyncTask responseAsyncTask = new PostResponseAsyncTask(this,hashMap);
+            responseAsyncTask.execute("https://10.0.3.2/Micro/studentsignup.php");
+
+*/       RadioButton radioButton=findViewById(radioGroup.getCheckedRadioButtonId());
+        dep=findViewById(R.id.departement);
+
+        String gender=radioButton.getText().toString();
+            String first=firstname.getText().toString();
+            String last=lastname.getText().toString();
+            String deps=dep.getSelectedItem().toString();
+            String secs=sec.getSelectedItem().toString();
+            String yer=year.getText().toString();
+            String user=id.getText().toString();
+            String pas=pass.getText().toString();
+          new Signup().execute(first,last,gender,deps,secs,yer,user,pas);
+
     }
 
-    @Override
-    public void processFinish(String s)
-    {
-        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+
+    private class Signup extends AsyncTask<String,String,String> {
+        URL url=null;
+        HttpURLConnection httpURLConnection;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                url=new URL("http://10.0.3.2/Micro/studentsignup.php");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            try {
+                httpURLConnection=(HttpURLConnection)url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setReadTimeout(10000);
+                httpURLConnection.setConnectTimeout(10000);
+                OutputStream outputStream=httpURLConnection.getOutputStream();
+                Uri.Builder builder=new Uri.Builder()
+                        .appendQueryParameter("First_name",strings[0])
+                        .appendQueryParameter("Last_name",strings[1])
+                        .appendQueryParameter("Gender",strings[2])
+                        .appendQueryParameter("Departement",strings[3])
+                        .appendQueryParameter("Year",strings[4])
+                        .appendQueryParameter("Section",strings[5])
+                        .appendQueryParameter("StudentId",strings[6])
+                        .appendQueryParameter("Password",strings[7]);
+                String qurey= builder.build().getEncodedQuery();
+                BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                writer.write(qurey);
+                writer.flush();
+                writer.close();
+                outputStream.close();
+                httpURLConnection.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                int response_code=httpURLConnection.getResponseCode();
+                if (response_code==HttpURLConnection.HTTP_OK){
+                    InputStream inputStream= httpURLConnection.getInputStream();
+                    BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder builder=new StringBuilder();
+                    String line;
+                    if (null!=(line=reader.readLine())){
+                        builder.append(line);
+                    }
+                    return builder.toString();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "exception";
+            }
+            return String.valueOf(httpURLConnection);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s.equalsIgnoreCase("Registered successfully")){
+                Toast.makeText(Student_Signup_Activity.this, "Registered sucessfully", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),Student_Login_Activity.class));
+                finish();
+            }else{
+                Toast.makeText(Student_Signup_Activity.this, "please fill the form first", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
